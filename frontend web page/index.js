c57c4414ac147a3f6719db027afe5fc4c5531885
Benrico Krog <62467535+benricok@users.noreply.github.com>
2022-04-29 14:57:14 +0200

    started dev of web page

---
+++ frontend web page/index.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector(".gCanvas");
+const ctx = canvas.getContext("2d");
+
+const testArray = [35,103,76,210,184,120];
+const scale = 20;
+
+function viewer() {
+    this.myPos.x = 190;
+    this.myPos.y = 300;
+    ctx.beginPath();    
+    
+    // start at point x=5 y=10      
+    ctx.moveTo( 5, 10 );     
+    // create line from point x=5 y=10 to x=45 y=50     
+    ctx.lineTo( 45, 50 );     
+    // draw path to canvas      
+    ctx.stroke();   
+
+    function updateScreen() {
+        var degrees = 15;
+        length
+        for (let i = 1; i < 5; i++) {
+            
+            degrees + 30;
+        }
+    }
+
+    function main() {
+        ctx.fillStyle = "#32CD32"
+        ctx.fillRect(myPos.x, myPos.y, scale, scale);
+    }
+    main();
+}
+viewer();
